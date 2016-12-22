cfda5689a87a66b970ed03e37c27272fc0461d45
tejpal.a.singh <tejpal.a.singh@BDC3-D-519QT01.dir.svc.accenture.com>
2016-12-22 16:24:23 +0530

    commit

---
+++ src/com/acc/Sirini.java
@@ -0,0 +1,13 @@
+package com.acc;
+
+public class Sirini {
+public String animal;
+
+public String getAnimal() {
+	return animal;
+}
+
+public void setAnimal(String animal) {
+	this.animal = animal;
+}
+}
